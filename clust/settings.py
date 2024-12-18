ace1456b0542cdb689cbcd6d93bb04328e912faf
araga2004 <arpitagarwal22042004@gmail.com>
2024-12-18 13:46:05 +0530

    some websocket functionality

--- clust/settings.py
+++ clust/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'channels',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -39,6 +40,8 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'base'
 ]
+
+ASGI_APPLICATION = 'clust.asgi.application'
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
